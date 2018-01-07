8f8f9a62e69fa541d5425b20b3d9d10f67b240fb
VolodymyrKladochnyi <kladochnyi.v@gmail.com>
2018-01-07 14:36:01 +0200

    added arrows to slider

--- src/js/app.js
+++ src/js/app.js
@@ -10,6 +10,6 @@ import 'lightslider';
 $('.twitter__slider').lightSlider({
   item: 1,
   pager: false,
-  prevHtml:'<div class="slider__arrow slider__arrow_prev"><img class="arrow__icon" src="././icons/prev.svg"></div>',
-  nextHtml:'<div class="slider__arrow slider__arrow_next"><img class="arrow__icon" src="././icons/next.svg"></div>'
+  prevHtml:'<div class="slider__arrow"><img class="arrow__icon" src="././icons/prev.svg"></div>',
+  nextHtml:'<div class="slider__arrow"><img class="arrow__icon" src="././icons/next.svg"></div>'
 });
